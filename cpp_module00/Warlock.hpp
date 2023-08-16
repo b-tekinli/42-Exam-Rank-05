a651098c042c5aa54bb5ee6ca09d9595160ba382
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-08-16 13:38:04 +0300

    added

---
+++ cpp_module00/Warlock.hpp
@@ -0,0 +1,26 @@
+#ifndef WARLOCK_HPP
+#define WARLOCK_HPP
+
+#include <iostream>
+
+class Warlock {
+    private:
+        std::string name;
+        std::string title;
+
+        Warlock();
+        Warlock(const Warlock& other);
+        Warlock& operator=(const Warlock& other);
+    
+    public:
+        Warlock(const std::string& name, const std::string& title);
+        ~Warlock();
+
+        const std::string& getName();
+        const std::string& getTitle();
+
+        void setTitle(const std::string& title);
+        void introduce() const;
+};
+
+#endif
